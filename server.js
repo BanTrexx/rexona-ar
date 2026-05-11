87f407c17469ea9ebee98d8ed19fb62cc3fcec84
Muhammad Faiz Satrio <faizsatrio890@gmail.com>
2026-05-11 21:46:06 +0700

    initial commit

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const path = require("path");
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.get("/booth", (req, res) => {
+  res.send("📸 Halaman Kamera belum dibuat cuy, semangat ngerjainnya!");
+});
+
+app.get("/api/status", (req, res) => {
+  res.status(200).json({
+    status: "success",
+    message: "Server Node.js jalan cuy!",
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server meluncur di http://localhost:${PORT}`);
+});
